fdedee6bcd3c108ad605352a1a1a97e8d474b863
Emjr31 <adonay.chacon12@gmail.com>
2021-09-20 21:35:48 -0600

    Creacion de formularios,funcionamiento de formulario ingreso

---
+++ DH2-G3/Ingreso.Designer.cs
@@ -0,0 +1,270 @@
+﻿namespace DH2_G3
+{
+    partial class Ingreso
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.Materia = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.txtNombre = new System.Windows.Forms.TextBox();
+            this.txtApellido = new System.Windows.Forms.TextBox();
+            this.txtMateria = new System.Windows.Forms.TextBox();
+            this.txtCarnet = new System.Windows.Forms.TextBox();
+            this.txtNota2 = new System.Windows.Forms.TextBox();
+            this.txtNota1 = new System.Windows.Forms.TextBox();
+            this.txtNota3 = new System.Windows.Forms.TextBox();
+            this.dgvAlumnos = new System.Windows.Forms.DataGridView();
+            this.brnSalir = new System.Windows.Forms.Button();
+            this.bntGuardar = new System.Windows.Forms.Button();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvAlumnos)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.bntGuardar);
+            this.groupBox1.Controls.Add(this.txtNota3);
+            this.groupBox1.Controls.Add(this.txtNota2);
+            this.groupBox1.Controls.Add(this.txtNota1);
+            this.groupBox1.Controls.Add(this.txtMateria);
+            this.groupBox1.Controls.Add(this.txtCarnet);
+            this.groupBox1.Controls.Add(this.txtApellido);
+            this.groupBox1.Controls.Add(this.txtNombre);
+            this.groupBox1.Controls.Add(this.label7);
+            this.groupBox1.Controls.Add(this.label6);
+            this.groupBox1.Controls.Add(this.label5);
+            this.groupBox1.Controls.Add(this.Materia);
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Location = new System.Drawing.Point(23, 22);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(727, 106);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Datos";
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.dgvAlumnos);
+            this.groupBox2.Location = new System.Drawing.Point(23, 147);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(727, 291);
+            this.groupBox2.TabIndex = 1;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Alumnos";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(35, 26);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(44, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Nombre";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(219, 26);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(38, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Carnet";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(35, 69);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(47, 13);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Apellido ";
+            // 
+            // Materia
+            // 
+            this.Materia.AutoSize = true;
+            this.Materia.Location = new System.Drawing.Point(215, 69);
+            this.Materia.Name = "Materia";
+            this.Materia.Size = new System.Drawing.Size(42, 13);
+            this.Materia.TabIndex = 3;
+            this.Materia.Text = "Materia";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(401, 26);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(42, 13);
+            this.label5.TabIndex = 4;
+            this.label5.Text = "Nota 1:";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(401, 69);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(45, 13);
+            this.label6.TabIndex = 5;
+            this.label6.Text = "Nota 2: ";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(567, 26);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(42, 13);
+            this.label7.TabIndex = 6;
+            this.label7.Text = "Nota 3:";
+            // 
+            // txtNombre
+            // 
+            this.txtNombre.Location = new System.Drawing.Point(91, 23);
+            this.txtNombre.Name = "txtNombre";
+            this.txtNombre.Size = new System.Drawing.Size(100, 20);
+            this.txtNombre.TabIndex = 7;
+            // 
+            // txtApellido
+            // 
+            this.txtApellido.Location = new System.Drawing.Point(91, 66);
+            this.txtApellido.Name = "txtApellido";
+            this.txtApellido.Size = new System.Drawing.Size(100, 20);
+            this.txtApellido.TabIndex = 8;
+            // 
+            // txtMateria
+            // 
+            this.txtMateria.Location = new System.Drawing.Point(277, 66);
+            this.txtMateria.Name = "txtMateria";
+            this.txtMateria.Size = new System.Drawing.Size(100, 20);
+            this.txtMateria.TabIndex = 10;
+            // 
+            // txtCarnet
+            // 
+            this.txtCarnet.Location = new System.Drawing.Point(277, 23);
+            this.txtCarnet.Name = "txtCarnet";
+            this.txtCarnet.Size = new System.Drawing.Size(100, 20);
+            this.txtCarnet.TabIndex = 9;
+            // 
+            // txtNota2
+            // 
+            this.txtNota2.Location = new System.Drawing.Point(461, 66);
+            this.txtNota2.Name = "txtNota2";
+            this.txtNota2.Size = new System.Drawing.Size(63, 20);
+            this.txtNota2.TabIndex = 12;
+            // 
+            // txtNota1
+            // 
+            this.txtNota1.Location = new System.Drawing.Point(461, 23);
+            this.txtNota1.Name = "txtNota1";
+            this.txtNota1.Size = new System.Drawing.Size(63, 20);
+            this.txtNota1.TabIndex = 11;
+            // 
+            // txtNota3
+            // 
+            this.txtNota3.Location = new System.Drawing.Point(615, 23);
+            this.txtNota3.Name = "txtNota3";
+            this.txtNota3.Size = new System.Drawing.Size(63, 20);
+            this.txtNota3.TabIndex = 13;
+            // 
+            // dgvAlumnos
+            // 
+            this.dgvAlumnos.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvAlumnos.Location = new System.Drawing.Point(91, 19);
+            this.dgvAlumnos.Name = "dgvAlumnos";
+            this.dgvAlumnos.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
+            this.dgvAlumnos.Size = new System.Drawing.Size(461, 266);
+            this.dgvAlumnos.TabIndex = 0;
+            this.dgvAlumnos.DoubleClick += new System.EventHandler(this.dgvAlumnos_DoubleClick);
+            // 
+            // brnSalir
+            // 
+            this.brnSalir.Location = new System.Drawing.Point(658, 453);
+            this.brnSalir.Name = "brnSalir";
+            this.brnSalir.Size = new System.Drawing.Size(75, 23);
+            this.brnSalir.TabIndex = 2;
+            this.brnSalir.Text = "Regresar";
+            this.brnSalir.UseVisualStyleBackColor = true;
+            // 
+            // bntGuardar
+            // 
+            this.bntGuardar.Location = new System.Drawing.Point(570, 66);
+            this.bntGuardar.Name = "bntGuardar";
+            this.bntGuardar.Size = new System.Drawing.Size(108, 23);
+            this.bntGuardar.TabIndex = 14;
+            this.bntGuardar.Text = "Guardar";
+            this.bntGuardar.UseVisualStyleBackColor = true;
+            this.bntGuardar.Click += new System.EventHandler(this.bntGuardar_Click);
+            // 
+            // Ingreso
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(773, 488);
+            this.Controls.Add(this.brnSalir);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "Ingreso";
+            this.Text = "Ingreso";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.dgvAlumnos)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.TextBox txtNota3;
+        private System.Windows.Forms.TextBox txtNota2;
+        private System.Windows.Forms.TextBox txtNota1;
+        private System.Windows.Forms.TextBox txtMateria;
+        private System.Windows.Forms.TextBox txtCarnet;
+        private System.Windows.Forms.TextBox txtApellido;
+        private System.Windows.Forms.TextBox txtNombre;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label Materia;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.DataGridView dgvAlumnos;
+        private System.Windows.Forms.Button brnSalir;
+        private System.Windows.Forms.Button bntGuardar;
+    }
+}
---
+++ DH2-G3/Ingreso.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DH2_G3
+{
+    public partial class Ingreso : Form
+    {
+        private List<alumno> Alumno = new List<alumno>();
+        private int indice = -1;
+        public Ingreso()
+        {
+            InitializeComponent();
+        }
+        //Actualizar la grid
+        private void actualizargrip()
+        {
+            dgvAlumnos.DataSource = null;
+            dgvAlumnos.DataSource = Alumno;
+        }
+        ///Limpiar
+        private void limpiar()
+        {
+            txtApellido.Clear();
+            txtCarnet.Clear();
+            txtMateria.Clear();
+            txtNombre.Clear();
+            txtNota1.Clear();
+            txtNota2.Clear();
+            txtNota3.Clear();
+            txtNombre.Focus();
+        }
+
+        private void dgvAlumnos_DoubleClick(object sender, EventArgs e)
+        {
+            DataGridViewRow seleccion = dgvAlumnos.SelectedRows[0];
+            indice = dgvAlumnos.Rows.IndexOf(seleccion);
+            alumno alum = Alumno[indice];
+            txtApellido.Text = alum.Apellido;
+            txtCarnet.Text = alum.Carnet;
+            txtMateria.Text = alum.Materia;
+            txtNombre.Text = alum.Nombre;
+            txtNota1.Text = alum.Calificaciones[0].ToString();
+            txtNota2.Text = alum.Calificaciones[1].ToString();
+            txtNota3.Text = alum.Calificaciones[2].ToString();
+        }
+
+        private void bntGuardar_Click(object sender, EventArgs e)
+        {
+            alumno alum = new alumno();
+            alum.Apellido = txtApellido.Text;
+            alum.Calificaciones[0] = float.Parse(txtNota1.Text);
+            alum.Calificaciones[1] = float.Parse(txtNota2.Text);
+            alum.Calificaciones[2] = float.Parse(txtNota3.Text);
+            alum.Nombre = txtNombre.Text;
+            alum.Carnet = txtCarnet.Text;
+            alum.Materia = txtMateria.Text;
+            if (indice > -1)
+            {
+                Alumno[indice] = alum;
+                indice = -1;
+            }
+            else
+            {
+                Alumno.Add(alum);
+            }
+            actualizargrip();
+            limpiar();
+        }
+    }
+}
